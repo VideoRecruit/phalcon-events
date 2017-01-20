38fc0c0aa8c1e0a83e4cd1aef7d294af251287af
Lukas Tomek <tomeklukas@gmail.com>
2017-01-20 14:16:48 +0100

    Exceptions refactoring.

--- src/Phalcon/Events/exceptions.php
+++ src/Phalcon/Events/exceptions.php
@@ -2,16 +2,89 @@
 
 namespace VideoRecruit\Phalcon\Events;
 
+interface IException
+{
+
+}
+
+
+
 /**
- * Common exception interface.
+ * The exception that is thrown when the value of an argument is
+ * outside the allowable range of values as defined by the invoked method.
  */
-interface Exception
+class ArgumentOutOfRangeException extends \InvalidArgumentException implements IException
+{
+
+}
+
+
+
+/**
+ * The exception that is thrown when a method call is invalid for the object's
+ * current state, method has been invoked at an illegal or inappropriate time.
+ */
+class InvalidStateException extends \RuntimeException implements IException
+{
+
+}
+
+
+
+/**
+ * The exception that is thrown when a requested method or operation is not implemented.
+ */
+class NotImplementedException extends \LogicException implements IException
+{
+
+}
+
+
+
+/**
+ * The exception that is thrown when an invoked method is not supported. For scenarios where
+ * it is sometimes possible to perform the requested operation, see InvalidStateException.
+ */
+class NotSupportedException extends \LogicException implements IException
+{
+
+}
+
+
+
+/**
+ * The exception that is thrown when accessing a class member (property or method) fails.
+ */
+class MemberAccessException extends \LogicException implements IException
 {
 }
 
+
+
 /**
- * Class InvalidArgumentException
+ * The exception that is thrown when an argument does not match with the expected value.
  */
-class InvalidArgumentException extends \InvalidArgumentException implements Exception
+class InvalidArgumentException extends \InvalidArgumentException implements IException
 {
+
+}
+
+
+
+/**
+ * The exception that is thrown when an illegal index was requested.
+ */
+class OutOfRangeException extends \OutOfRangeException implements IException
+{
+
+}
+
+
+
+/**
+ * The exception that is thrown when a value (typically returned by function) does not match with the expected value.
+ */
+class UnexpectedValueException extends \UnexpectedValueException implements IException
+{
+
 }
