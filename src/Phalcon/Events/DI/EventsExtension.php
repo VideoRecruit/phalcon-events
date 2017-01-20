94800820553fefa3b79cdded05fa5c13b62f5e0b
Lukas Tomek <tomeklukas@gmail.com>
2017-01-20 14:22:52 +0100

    Events extension creates event map and optimizes listeners:
     - this allow to listeners to be instantiate by event manager at the time they are required

--- src/Phalcon/Events/DI/EventsExtension.php
+++ src/Phalcon/Events/DI/EventsExtension.php
@@ -2,10 +2,14 @@
 
 namespace VideoRecruit\Phalcon\Events\DI;
 
-use Kdyby\Events\EventManager;
+use Doctrine\Common\EventSubscriber;
+use Kdyby\Events\Event;
 use Phalcon\Config;
 use VideoRecruit\Phalcon\DI\Container;
+use VideoRecruit\Phalcon\Events\EventManager;
 use VideoRecruit\Phalcon\Events\InvalidArgumentException;
+use VideoRecruit\Phalcon\Events\InvalidStateException;
+use VideoRecruit\Phalcon\Events\MemberAccessException;
 
 /**
  * Class EventsExtension
@@ -21,6 +25,11 @@ class EventsExtension
 	 * @var Container
 	 */
 	private $di;
+
+	/**
+	 * @var array
+	 */
+	private $listeners = [];
 
 	/**
 	 * DoctrineOrmExtension constructor.
@@ -58,6 +67,8 @@ class EventsExtension
 
 	/**
 	 * @param array $config
+	 * @throws InvalidStateException
+	 * @throws MemberAccessException
 	 */
 	private function loadManager(array $config)
 	{
@@ -65,14 +76,73 @@ class EventsExtension
 		$tagName = self::TAG_SUBSCRIBER;
 
 		$container->setShared(self::EVENT_MANAGER, function () use ($container, $tagName) {
-			$services = $container->getServicesByTag($tagName);
-			$eventsManager = new EventManager;
+			foreach ($services = $container->getServicesByTag($tagName) as $service) {
+				if (!is_array($service->getDefinition())) {
+					throw new InvalidStateException(sprintf('Listeners have to be defined using an array syntax. %s listener is defined in different way.', $service->getName()));
+				}
 
-			foreach ($services as $service) {
-				$eventsManager->addEventSubscriber($service->resolve());
+				$serviceName = $service->getName();
+				$eventNames = [];
+				$listenerClassName = $service->getDefinition()['className'];
+				$listener = self::createInstanceWithoutConstructor($listenerClassName);
+
+				// exception message
+				$msg = 'Event listener %s::%s() is not implemented.';
+
+				// create event map
+				foreach ($listener->getSubscribedEvents() as $eventName => $params) {
+					if (is_numeric($eventName) && is_string($params)) { // [EventName, ...]
+						list(, $method) = Event::parseName($params);
+						$eventNames[] = ltrim($params, '\\');
+
+						if (!method_exists($listener, $method)) {
+							throw new MemberAccessException(sprintf($msg, $listenerClassName, $method));
+						}
+					} elseif (is_string($eventName)) { // [EventName => ???, ...]
+						$eventNames[] = ltrim($eventName, '\\');
+
+						if (is_string($params)) { // [EventName => method, ...]
+							if (!method_exists($listener, $params)) {
+								throw new MemberAccessException(sprintf($msg, $listenerClassName, $params));
+							}
+						} elseif (is_string($params[0])) { // [EventName => [method, priority], ...]
+							if (!method_exists($listener, $params[0])) {
+								throw new MemberAccessException(sprintf($msg, $listenerClassName, $params[0]));
+							}
+						} else {
+							foreach ($params as $listener) { // [EventName => [[method, priority], ...], ...]
+								if (!method_exists($listener, $listener[0])) {
+									throw new MemberAccessException(sprintf($msg, $listenerClassName, $listener[0]));
+								}
+							}
+						}
+					}
+				}
+
+				$this->listeners[$serviceName] = array_unique($eventNames);
+				$listeners = [];
+
+				// optimize listeners
+				foreach ($this->listeners as $serviceName => $eventNames) {
+					foreach ($eventNames as $eventName) {
+						$listeners[$eventName][] = $serviceName;
+					}
+				}
+				foreach ($listeners as $id => $subscribers) {
+					$listeners[$id] = array_unique($subscribers);
+				}
 			}
 
-			return $eventsManager;
+			return new EventManager($listeners, $this);
 		});
+	}
+
+	/**
+	 * @param string $className
+	 * @return EventSubscriber
+	 */
+	private static function createInstanceWithoutConstructor($className)
+	{
+		return (new \ReflectionClass($className))->newInstanceWithoutConstructor();
 	}
 }
